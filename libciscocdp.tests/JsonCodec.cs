de483b33536568e21d809ae521dcbf1d6bd2a722
Darren R. Starr <darren@nocturnal.no>
2018-06-29 14:57:21 +0200

    "internal set" doesn't work well with NewtonSoft

---
+++ libciscocdp.tests/JsonCodec.cs
@@ -0,0 +1,108 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Xunit;
+
+namespace libciscocdp.tests
+{
+    public class JsonCodec
+    {
+        public static readonly string Sample1 = @"
+{
+    ""receivedOn"": ""eth1"",
+    ""receivedAt"": ""2018-06-29T13:37:18.3550893+02:00"",
+    ""sourceMac"": ""00155D1C8E0C"",
+    ""packet"": {
+        ""version"": 2,
+        ""ttl"": 180,
+        ""deviceId"": ""csr.log.local"",
+        ""softwareVersion"": ""Cisco IOS Software [Fuji], Virtual XE Software (X86_64_LINUX_IOSD-UNIVERSALK9-M), Version 16.7.2, RELEASE SOFTWARE (fc3)\nTechnical Support: http://www.cisco.com/techsupport\nCopyright (c) 1986-2018 by Cisco Systems, Inc.\nCompiled Tue 29-May-18 19:23 by mcpre"",
+        ""platform"": ""cisco CSR1000V"",
+        ""addresses"": [
+            ""192.168.70.3"",
+            ""2001:db8:70::3"",
+            ""fe80::215:5dff:fe1c:8e0c""
+        ],
+        ""portId"": ""GigabitEthernet2"",
+        ""hasCapabilities"": true,
+        ""canRoute"": true,
+        ""canTransparentBridge"": false,
+        ""canSourceRouteBridge"": false,
+        ""canSwitch"": false,
+        ""isHost"": false,
+        ""isIGMPCapable"": true,
+        ""isRepeater"": false,
+        ""duplex"": 1,
+        ""managementAddresses"": [
+            ""192.168.70.3""
+        ],
+        ""odrPrefixes"": [
+            {
+                ""network"": ""10.90.90.0"",
+                ""length"": 24
+            },
+            {
+                ""network"": ""172.21.61.96"",
+                ""length"": 28
+            }
+        ]
+    }
+}
+";
+
+        /// <summary>
+        /// Message model transmitted via REST to the server
+        /// </summary>
+        public class CdpApiMessage
+        {
+            /// <summary>
+            /// The interface name which the packet was received on
+            /// </summary>
+            public string ReceivedOn { get; set; }
+
+            /// <summary>
+            /// The date and time which the packet was received
+            /// </summary>
+            public DateTimeOffset ReceivedAt { get; set; }
+
+            /// <summary>
+            /// The transmitting device's source MAC address
+            /// </summary>
+            public string SourceMac { get; set; }
+
+            /// <summary>
+            /// The contents of the parsed packet
+            /// </summary>
+            public CdpPacket Packet { get; set; }
+        }
+
+        [Fact]
+        public void DecodePacket()
+        {
+            ITraceWriter traceWriter = new MemoryTraceWriter { LevelFilter = System.Diagnostics.TraceLevel.Verbose };
+
+            JsonSerializerSettings serializerSettings =
+                           new JsonSerializerSettings()
+                           {
+                               MissingMemberHandling = MissingMemberHandling.Ignore,
+                               DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+                               TraceWriter = traceWriter
+                           };
+
+            serializerSettings.Converters.Add(new IPAddressConverter());
+            serializerSettings.Converters.Add(new IPEndPointConverter());
+
+            var message = JsonConvert.DeserializeObject<CdpApiMessage>(Sample1, serializerSettings);
+
+            //throw new Exception(traceWriter.ToString());
+
+            Assert.NotNull(message);
+            Assert.NotNull(message.Packet);
+            Assert.NotNull(message.Packet.DeviceId);
+            Assert.NotEmpty(message.Packet.DeviceId);
+        }
+    }
+}
--- libciscocdp/CdpPacket.cs
+++ libciscocdp/CdpPacket.cs
@@ -11,37 +11,37 @@
         /// <summary>
         /// The version of the CDP packet
         /// </summary>
-        public int Version { get; internal set; }
+        public int Version { get; set; }
 
         /// <summary>
         /// The TTL value for the packet in seconds
         /// </summary>
-        public int Ttl { get; internal set; }
+        public int Ttl { get; set; }
 
         /// <summary>
         /// The device ID as reported by the device.
         /// </summary>
-        public string DeviceId { get; internal set; }
+        public string DeviceId { get; set; }
 
         /// <summary>
         /// The software version running on the device
         /// </summary>
-        public string SoftwareVersion { get; internal set; }
+        public string SoftwareVersion { get; set; }
 
         /// <summary>
         /// The platform reported by the device
         /// </summary>
-        public string Platform { get; internal set; }
+        public string Platform { get; set; }
 
         /// <summary>
         /// The addresses configured on the device
         /// </summary>
-        public List<IPAddress> Addresses { get; internal set; }
+        public List<IPAddress> Addresses { get; set; }
 
         /// <summary>
         /// The port on the device which the message was transmitted from
         /// </summary>
-        public string PortId { get; internal set; }
+        public string PortId { get; set; }
 
         /// <summary>
         /// True if capabilities are provided by the transmitter (not part of the packet)
@@ -51,81 +51,81 @@
         /// <summary>
         /// Can the device operate as a Layer-3 router?
         /// </summary>
-        public bool CanRoute { get; internal set; }
+        public bool CanRoute { get; set; }
 
         /// <summary>
         /// Can the device operate as a Layer-2 transparent bridge (uncommon)
         /// </summary>
-        public bool CanTransparentBridge { get; internal set; }
+        public bool CanTransparentBridge { get; set; }
 
         /// <summary>
         /// Can the device operate as a source routing bridge (uncommon)
         /// </summary>
-        public bool CanSourceRouteBridge { get; internal set; }
+        public bool CanSourceRouteBridge { get; set; }
 
         /// <summary>
         /// Can the device operate as a layer-2 switch
         /// </summary>
-        public bool CanSwitch { get; internal set; }
+        public bool CanSwitch { get; set; }
 
         /// <summary>
         /// Is the device a host or endpoint
         /// </summary>
-        public bool IsHost { get; internal set; }
+        public bool IsHost { get; set; }
 
         /// <summary>
         /// Is the device capable of processing or forwarding IGMP packets
         /// </summary>
-        public bool IsIGMPCapable { get; internal set; }
+        public bool IsIGMPCapable { get; set; }
 
         /// <summary>
         /// Is the device a network repeater
         /// </summary>
-        public bool IsRepeater { get; internal set; }
+        public bool IsRepeater { get; set; }
 
         /// <summary>
         /// The CDP Hello Protocol
         /// </summary>
-        public CdpHelloProtocol HelloProtocol { get; internal set; }
+        public CdpHelloProtocol HelloProtocol { get; set; }
 
         /// <summary>
         /// The native VLAN reported by the device for the link
         /// </summary>
-        public int? NativeVlan { get; internal set; }
+        public int? NativeVlan { get; set; }
 
         /// <summary>
         /// The VTP management domain, this can be empty
         /// </summary>
-        public string VtpManagementDomain { get; internal set; }
+        public string VtpManagementDomain { get; set; }
 
         /// <summary>
         /// The duplex the transmitting device is operating on for this link
         /// </summary>
-        public ECdpDuplex? Duplex { get; internal set; }
+        public ECdpDuplex? Duplex { get; set; }
 
         /// <summary>
         /// Trust bitmap (not sure what this is)
         /// </summary>
-        public int? TrustBitmap { get; internal set; }
+        public int? TrustBitmap { get; set; }
 
         /// <summary>
         /// Untrusted Port Class of Service (not sure what this is)
         /// </summary>
-        public int? UntrustedPortCoS { get; internal set; }
+        public int? UntrustedPortCoS { get; set; }
 
         /// <summary>
         /// The management addresses of the device
         /// </summary>
-        public List<IPAddress> ManagementAddresses { get; internal set; }
+        public List<IPAddress> ManagementAddresses { get; set; }
 
         /// <summary>
         /// Power of Ethernet availability information
         /// </summary>
-        public CdpPowerAvailable PowerAvailable { get; internal set; }
+        public CdpPowerAvailable PowerAvailable { get; set; }
 
         /// <summary>
         /// IPv4 Prefixes provided for IPv4 "On-Demand Routing"
         /// </summary>
-        public List<IPPrefix> OdrPrefixes { get; internal set; }
+        public List<IPPrefix> OdrPrefixes { get; set; }
     }
 }
